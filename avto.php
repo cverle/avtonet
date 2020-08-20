795a90b53949739c78cd040a21577ed637a2df0c
TrojanLij <1sk8rdude@gmail.com>
2020-08-20 12:05:18 +0200

    added profile

--- avto.php
+++ avto.php
@@ -11,21 +11,35 @@
   <section class="py-5">
     <div class="container-fluid" style="padding-top:10%;">
       <div class="container">
-        <div class="btn-group">
-          <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Brands <span class="sr-only">Toggle Dropdown</span>
+        <form>
+          <div class="form-group">
+            <div class="container-fluid">
+              <h3>Brands</h3>
+            </div>
+            <div class="container-fluid">
+              <?php
+                $select_brand="SELECT brands.brand AS `brand` FROM `brands` ORDER BY brand DESC;";
+                $stmt=$pdo->query($select_brand);
+                while($brand=$stmt->fetch()){
+                  $selected_brand=$brand['brand'];
+                  echo '
+                  <label class="checkbox-inline">
+                    <input type="checkbox" value="'.strval($selected_brand).'">'.strval($selected_brand).'
+                  </label>';
+                } 
+              ?>
+            </div>
+          </div>
+        </form>
+        <!-- <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Brands <span class="sr-only">Toggle Dropdown</span>
           </button>
           <div class="dropdown-menu">
-          <?php
-          $select_brand="SELECT brands.brand AS `brand` FROM `brands` ORDER BY brand DESC;";
-          $stmt=$pdo->query($select_brand);
-          while($brand=$stmt->fetch()){
-            $selected_brand=$brand['brand'];
-            echo'<a class="dropdown-item" href="#">'.strval($selected_brand).'</a>';
-          } 
-          ?>
+            
           </div>
-        </div>
+        </div> -->
         <?php 
           $select_all="SELECT * FROM users";
           $stmt=$pdo->query($select_all);
---
+++ profile.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require('css-js.php'); ?>
+</head>
+
+<body>
+
+    <?php include('header.php'); ?>
+    <section class="py-5">
+        <div class="container-fluid" style="padding-top:10%;">
+            <div class="container">
+                <h3>Add a car:</h3>
+                <hr />
+                <form action="" method="POST">
+                    <div class="form-group row">
+                        <label for="brandsList" class="col-sm-2 col-form-label">Brands</label>
+                        <div class="col-sm-10">
+                            <?php
+                                $select_brand="SELECT brands.brand AS `brand` FROM `brands` ORDER BY brand DESC;";
+                                $stmt=$pdo->query($select_brand);
+                                if($stmt->rowCount() > 0){
+                                    echo '<select class="form-control" id="brandsList">';
+                                    while($brand=$stmt->fetch()){
+                                    $selected_brand=$brand['brand'];
+                                    echo '                               
+                                        <option>'.strval($selected_brand).'</option>
+                                    ';
+                                    }
+                                    echo '</select>';
+                                }else{
+                                    echo '<select class="form-control" id="brandsList" disabled>
+                                    </select>';
+                                }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="brandsList" class="col-sm-2 col-form-label">Models</label>
+                        <div class="col-sm-10">
+                            <?php
+                                $select_models="SELECT models.model AS `models` FROM models ORDER BY models.model DESC;";
+                                $stmt=$pdo->query($select_models);
+                                if($stmt->rowCount() > 0){
+                                    echo '<select class="form-control" id="modelsList">';
+                                    while($models=$stmt->fetch()){
+                                    $selected_models=$models['models'];
+                                    echo '                               
+                                        <option>'.strval($selected_models).'</option>
+                                    ';
+                                    }
+                                    echo '</select>';
+                                }else{
+                                    echo '<select class="form-control" id="modelsList" disabled>
+                                    </select>';
+                                }
+                            ?>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <?php include('footer.php'); ?>
+</body>
+
+</html>
